5db5fc174485cb1f7d34ee31b1e1ff0fe4aa6ac3
Nat <wmainvielle@gmail.com>
2021-07-14 14:50:12 +0200

    Create hug.js

---
+++ discord.js/bots/basic bot/commands/hug.js
@@ -0,0 +1,18 @@
+const { Message } = require('discord.js')
+
+const hugs = ['https://tenor.com/boXzB.gif', 'https://tenor.com/biszv.gif']
+
+module.exports = {
+	/** 
+     * @param { object } param0  
+     * @param { Message } [param0.message]  
+     * @param { string[] } [param0.args]  
+     */
+	exec: ({ message, args }) => {
+		const members = message.guild.members.cache
+		const search = args.join(' ')
+		const member = members.get(search.replace(/\D+/g, '')) || members.find((m) => m.id == search.replace(/\D+/g, '') || m.user.tag.toLowerCase().includes(search.toLowerCase()) || m.displayName.toLowerCase().includes(search.toLowerCase())) || message.member
+		message.channel.send({ embed: { color: 'random', description: `❤ ${message.member} fait un calin à ${member}.`, image: { url: hugs[Math.floor(Math.random() * hugs.length)] } } })
+	},
+	options: { name: 'hug' }
+}
