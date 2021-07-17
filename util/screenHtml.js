4c2c37c9adf066a243762971b18d7f546bfe9b5d
Nat <wmainvielle@gmail.com>
2021-07-17 17:45:22 +0200

    Create screenHtml.js

---
+++ util/screenHtml.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+
+/** 
+ * @async
+ * @param { string } url 
+ * @param { object } param1 
+ * @param { 'image' | 'pdf' } [param1.type] 
+ * @param { { width: number, height: number } } [param1.view] 
+ * @param { string } [param1.path] 
+ * @example
+ * await screenHtlm(url) // Give screen of HTML page
+ * await screenHtml(url, { type: 'pdf' }) // Give a pdf of HTML page
+ * @returns Promise<string | Buffer>
+ */
+async function screenHtml (url, { type = 'image', view = { width: 1920, height: 1080 }, path } = {}) {
+	if (typeof url !== 'string') throw TypeError('url must be a string')
+	const browser = await puppeteer.launch({ defaultViewport: view })
+	const page = await browser.newPage()
+	await page.goto(url)
+	const result = type === 'image' ? await page.screenshot({ fullPage: true, type: 'png', path }) : type === 'pdf' ? await page.pdf({ path, format: 'a4' }) : null
+	await browser.close()
+	return result
+}
