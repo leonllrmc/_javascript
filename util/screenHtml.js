5c22941cd8548c26d750e1a82e19c42df89b4da8
leonllrmc <leonllrcraft@gmail.com>
2021-07-21 20:42:36 +0200

    Update screenHtml.js

--- util/screenHtml.js
+++ util/screenHtml.js
@@ -13,11 +13,15 @@ const puppeteer = require('puppeteer')
  * @returns Promise<string | Buffer>
  */
 async function screenHtml (url, { type = 'image', view = { width: 1920, height: 1080 }, path } = {}) {
-	if (typeof url !== 'string') throw TypeError('url must be a string')
-	const browser = await puppeteer.launch({ defaultViewport: view })
-	const page = await browser.newPage()
-	await page.goto(url)
-	const result = type === 'image' ? await page.screenshot({ fullPage: true, type: 'png', path }) : type === 'pdf' ? await page.pdf({ path, format: 'a4' }) : null
-	await browser.close()
-	return result
+	try {
+		if (typeof url !== 'string') throw TypeError('url must be a string')
+		const browser = await puppeteer.launch({ defaultViewport: view })
+		const page = await browser.newPage()
+		await page.goto(url)
+		const result = type === 'image' ? await page.screenshot({ fullPage: true, type: 'png', path }) : type === 'pdf' ? await page.pdf({ path, format: 'a4' }) : null
+		
+		return result
+	}finally {
+		await browser.close()
+	}
 }
